bcc757101f416567635f7dfceeb708a914084479
Elhadji Amadou Oury Diallo <elamadoury@gmail.com>
2019-11-01 12:36:32 +0900

    Get all the information of a user

--- src/store.js
+++ src/store.js
@@ -31,7 +31,10 @@ export default new Vuex.Store({
             if (user) {
                 commit("SET_USER", {
                     displayName: user.displayName, // ??? Don't know the properties of a user.
-                    email: user.email
+                    email: user.email,
+                    uid: user.uid,
+                    emailVerified: user.emailVerified,
+                    photoUrl: user.photoUrl
                 });
             } else {
                 commit("SET_USER", null);
