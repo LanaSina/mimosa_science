980c8cbb0e095abbc754803528491c7f18c3b8c4
Elhadji Amadou Oury Diallo <elamadoury@gmail.com>
2020-01-24 15:42:12 +0900

    Add a cloud function to get the a given user's information

--- functions/index.js
+++ functions/index.js
@@ -10,3 +10,17 @@ const functions = require('firebase-functions');
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
 admin.initializeApp();
+
+
+exports.getUserInfo = functions.https.onCall((uid) => {
+    admin.auth().getUser(uid)
+        .then(userRecord => {
+            // See the UserRecord reference doc for the contents of userRecord.
+            console.log('Successfully fetched user data:', userRecord.toJSON());
+            return userRecord.toJSON().displayName;
+        })
+        .catch(error => {
+            console.log('Error fetching user data:', error);
+            return error;
+        })
+});
