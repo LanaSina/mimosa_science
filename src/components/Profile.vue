f001652c9200ac707c6b8bc78a60b706e6b56820
Elhadji Amadou Oury Diallo <elamadoury@gmail.com>
2020-02-03 16:03:00 +0900

    Combine all update components into one view

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -2,17 +2,33 @@
     <div class="container">
         <b-tabs content-class="mt-3">
             <b-tab title="Update Profile" active> 
-
+                <UpdateProfile/>
             </b-tab>
 
             <b-tab title="Update Email">
-
+                <UpdateEmail/>
             </b-tab>
 
             <b-tab title="Update Password">
-
+                <UpdatePassword/>
             </b-tab>
         </b-tabs>
     </div>
 </template>
+
+<script>
+import UpdateEmail from '@/components/UpdateEmail.vue'
+import UpdateProfile from '@/components/UpdateProfile.vue'
+import UpdatePassword from '@/components/UpdatePassword.vue'
+
+export default {
+    name: 'Profile',
+
+    components: {
+        UpdateEmail,
+        UpdatePassword,
+        UpdateProfile
+    }
+}
+</script>
             
