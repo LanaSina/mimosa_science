da7b891825f05e1f056ba463001adc2024919281
Elhadji Amadou Oury Diallo <elamadoury@gmail.com>
2020-03-02 12:59:46 +0900

    Restrict the upload format to jpg ang png

--- src/components/UploadImage.vue
+++ src/components/UploadImage.vue
@@ -6,6 +6,7 @@
       :state="Boolean(file)"
       placeholder="Choose a file or drop it here..."
       drop-placeholder="Drop file here..."
+      accept=".jpg, .png"
     ></b-form-file>
     <div class="mt-3">Selected file: {{ file ? file.name : '' }}</div>
   </div>
