5388cb163760b6ff712c9ebeb4bb3a79d05cd7d4
Elhadji Amadou Oury Diallo <elamadoury@gmail.com>
2020-03-04 14:40:26 +0900

    Change the 'add experiment to a toggle collapse and modal

--- src/views/Hypothesis.vue
+++ src/views/Hypothesis.vue
@@ -34,8 +34,21 @@
 			</div>
 
 			<ExperimentsList v-if="hypothesis && question" v-bind:question='question' v-bind:hypothesis='hypothesis'/>
+			<h2>blabla</h2>
+			
+			<b-button v-b-modal="'my-modal'">Add your hypothesis (Modal)</b-button>
 
-			<Form v-bind:question='question' v-bind:hypothesis='hypothesis'/>
+			<b-modal id="my-modal" size="xl" title="Add your hypothesis">
+				<Form v-bind:question='question' v-bind:hypothesis='hypothesis'/>
+			</b-modal>
+			
+			<br/><b-button v-b-toggle.collapse>Add your hypothesis (Toggle Collapse)</b-button>
+
+			<b-collapse id="collapse">
+				<Form v-bind:question='question' v-bind:hypothesis='hypothesis'/>
+			</b-collapse>
+
+			<!-- <Form v-bind:question='question' v-bind:hypothesis='hypothesis'/> -->
 		</div>
 	</div>
 </template>
